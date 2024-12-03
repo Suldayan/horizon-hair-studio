48f9a2b4169c5974c9e3ecea2abef8eff9190a03
Timothy <timothysuldayan1@gmail.com>
2024-12-02 20:56:04 -0800

    added appointment microservice

---
+++ horizonhairstudio-appointment-service/src/main/java/com/example/horizonhairstudio_appointment_service/HorizonhairstudioAppointmentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.horizonhairstudio_appointment_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HorizonhairstudioAppointmentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HorizonhairstudioAppointmentServiceApplication.class, args);
+	}
+
+}
---
+++ horizonhairstudio-appointment-service/src/test/java/com/example/horizonhairstudio_appointment_service/HorizonhairstudioAppointmentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.horizonhairstudio_appointment_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HorizonhairstudioAppointmentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
