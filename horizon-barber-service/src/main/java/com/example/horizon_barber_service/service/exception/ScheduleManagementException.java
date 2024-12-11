7405682221f643869b5d78c7dc4a95864eb4700e
Timothy <timothysuldayan1@gmail.com>
2024-12-10 20:11:03 -0800

    added custom exception class for schedule management service

---
+++ horizon-barber-service/src/main/java/com/example/horizon_barber_service/service/exception/ScheduleManagementException.java
@@ -0,0 +1,7 @@
+package com.example.horizon_barber_service.service.exception;
+
+public class ScheduleManagementException extends Exception {
+    public ScheduleManagementException(String msg) {
+        super(msg);
+    }
+}
